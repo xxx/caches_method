d29e0eb23c5f356b7846ba5d970c0c5585ec3296
mpd <michael@tennmandigital.com>
2010-02-16 18:18:33 -0800

    whitespace fixen

--- lib/caches_method.rb
+++ lib/caches_method.rb
@@ -1,25 +1,22 @@
-
 module CachesMethod
   def self.included(base)
     base.extend ClassMethods
   end
 
   module ClassMethods
+    def caches_method(method, cache_options = {})
+      alias_method "uncached_#{method}", method
 
-  def caches_method(method, cache_options = {})
-    alias_method "uncached_#{method}", method
+      define_method method do |*args|
+        key = cache_key + "/#{method}/#{args}"
+        result = Rails.cache.read(key)
+        return result if result
 
-    define_method method do |*args|
-      key = cache_key + "/#{method}/#{args}"
-      result = Rails.cache.read(key)
-      return result if result
-
-      result = send("uncached_#{method}", *args)
-      Rails.cache.write(key, result, cache_options)
-      result
+        result = send("uncached_#{method}", *args)
+        Rails.cache.write(key, result, cache_options)
+        result
+      end
     end
-  end
-
   end
 end
 
